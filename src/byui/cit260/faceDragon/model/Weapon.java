ea9a0d7af373979aeb3812d8439c7f38c5b693e0
Justin <Justin@10.18.146.18>
2016-01-28 10:01:01 -0700

    1/27/2016: Initial commit to repository 1/28/2016: Edited 1/28/2016: Added InventoryItems 1/28/2016: Added Weapon Class

---
+++ src/byui/cit260/faceDragon/model/Weapon.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.faceDragon.model;
+
+/**
+ *
+ * @author Justin
+ */
+public class Weapon extends InventoryItems {
+    private double strength;
+
+    public Weapon() {
+        super();
+    }
+
+    
+    
+    
+    
+    
+    public double getStrength() {
+        return strength;
+    }
+
+    public void setStrength(double strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + (int) (Double.doubleToLongBits(this.strength) ^ (Double.doubleToLongBits(this.strength) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{" + "strength=" + strength + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Weapon other = (Weapon) obj;
+        return Double.doubleToLongBits(this.strength) == Double.doubleToLongBits(other.strength);
+    }
+    
+    
+    
+}
