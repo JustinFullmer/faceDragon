780596169f6e5e6ff709987a68990b121a40d2df
breecarrick <breecarrick@10.18.148.204>
2016-01-28 09:50:57 -0700

    01/28/16 imported Game class and tested it 01/28/16 Created Map Class

---
+++ src/byui/cit260/faceDragon/model/Map.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.faceDragon.model;
+
+import java.io.Serializable;
+/**
+ *
+ * @author breecarrick
+ */
+public class Map implements Serializable{
+    //class instance variables
+    private double rowCount;
+    private double columnCount;
+    
+    //constructor function
+
+    public Map() {
+    }
+    
+    //getter and setter functions
+    public double getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(double rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public double getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(double columnCount) {
+        this.columnCount = columnCount;
+    }
+ 
+    //hashcode and equals
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.rowCount) ^ (Double.doubleToLongBits(this.rowCount) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.columnCount) ^ (Double.doubleToLongBits(this.columnCount) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (Double.doubleToLongBits(this.rowCount) != Double.doubleToLongBits(other.rowCount)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.columnCount) != Double.doubleToLongBits(other.columnCount)) {
+            return false;
+        }
+        return true;
+    }
+    
+    //to string function
+
+    @Override
+    public String toString() {
+        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + '}';
+    }
+    
+    
+}
