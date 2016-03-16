37bdfe54e8ceca556802b3378767d03629af25bf
Justin <Justin@10.18.146.18>
2016-03-16 11:37:42 -0600

    3/16:GameControl viewMenu, GameControl InventoryItems[] sortItems

--- src/byui/cit260/faceDragon/control/MapControl.java
+++ src/byui/cit260/faceDragon/control/MapControl.java
@@ -27,7 +27,7 @@ public class MapControl {
           }
 
     static void moveCharactersToStartingLocation(Map map) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("**** moveCharactersToStartingLocation funciont called*************");
     }
 
     //static class createMap extends Map {
